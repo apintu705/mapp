b12c90752c7acfb8b511b15fa1b767961dc7b7b0
apintu705 <apintu705@gmail.com>
2022-08-31 13:57:15 +0530

    a

--- server/index.js
+++ server/index.js
@@ -18,9 +18,9 @@ app.use("/",router);
 app.get("/",(req,res)=>{res.send("hi")})
 
 const PORT=process.env.PORT || 8080
-app.listen(PORT,()=>{
+app.listen(process.env.PORT,()=>{
     connect();
-    console.log(`listening on port ${PORT}`);
+    console.log(`listening on port ${process.env.PORT}`);
 })
 
 
